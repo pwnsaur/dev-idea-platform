c3d8b72642d43e4b9fd6df99fbaea06ad9bd948b
DragonSky_NG <dragonskyng@gmail.com>
2022-09-21 14:36:30 +0300

    Complete first iteratio of post controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import { set } from "mongoose";
 
