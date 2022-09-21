abdcd89fdcc4be33b1fba8c09aea86336981b342
DragonSky_NG <dragonskyng@gmail.com>
2022-09-21 14:14:43 +0300

    Complete first iteration of authentication validation

--- server/authValidation/authValidation.js
+++ server/authValidation/authValidation.js
@@ -1,10 +1,23 @@
 import jwt from "jsonwebtoken";
-import userModel from "../models/userModel";
 
-const validatePerson=(req,res,next)=>{
-  
-}
+const validateUser = (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    next();
+  } else {
+    return res.status(406).send("You don't have necessary access");
+  }
+};
 
-export const validateSessionToken = (req,res,next)=>{
-  
-}
+export const validateSessionToken = (req, res, next) => {
+  const token = req.cookie.session_token;
+  if (!token) {
+    return res.status(401).send("Not authorized!");
+  }
+  jwt.verify(token, process.env.JWT_SECRET, async (error, decoded) => {
+    if (error) {
+      return res.status(404).send("Token is invalid!");
+    }
+    req.user = decoded;
+    validateUser(req, res, next);
+  });
+};
