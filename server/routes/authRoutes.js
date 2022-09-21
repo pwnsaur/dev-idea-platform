ebb0f01aa527c0be5feca3937a17c4078e811410
DragonSky_NG <dragonskyng@gmail.com>
2022-09-21 14:40:01 +0300

    Add controllers to routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/login");
-router.post("/register");
-router.post("/logout");
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 export default router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,11 +1,18 @@
-import express from "express"
+import express from "express";
+import { validateSessionToken } from "../authValidation/authValidation";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  updatePost,
+} from "../controllers/postController";
 import router from "./authRoutes";
 
 const route = express.Router();
 
-route.get("/get");
-router.put("/update/:id");
-router.delete("/delete/:id");
-
+router.post("/create", validateSessionToken, createPost);
+route.get("/get", getPosts);
+router.put("/update/:id", validateSessionToken, updatePost);
+router.delete("/delete/:id", validateSessionToken, deletePost);
 
 export default router;
