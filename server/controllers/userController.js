36cae2c9efebf525f91afeb53ec3f5c465ae9d4f
DragonSky_NG <dragonskyng@gmail.com>
2022-09-22 17:31:20 +0300

    Initial user route and controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+import userModel from "../models/userModel.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find({}, { password: 0 });
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send(error);
+  }
+};
