393857a449b6788f2060a69b29845200eef55540
DragonSky_NG <dragonskyng@gmail.com>
2022-09-23 16:19:43 +0300

    add user routes and add extra validation to model fields

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {getUsers} from "../controllers/userController.js"
+import {getUsers, updateUser} from "../controllers/userController.js"
 const router = express.Router();
 
 router.get("/get", getUsers)
+router.put("/update/:id", updateUser)
 
 export default router;
