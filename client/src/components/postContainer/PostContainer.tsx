161971ae2b87e68e60d8ac2f49ddd85d78d8f43b
DragonSky_NG <dragonskyng@gmail.com>
2022-09-24 15:19:12 +0300

    Swtich data from json to context in PostWindow.tsx

--- client/src/components/postContainer/PostContainer.tsx
+++ client/src/components/postContainer/PostContainer.tsx
@@ -1,6 +1,14 @@
 import styles from './postContainer.module.scss';
 import { IPost } from '../../interfaces/post';
-interface Props extends IPost {
+interface Post {
+  _id: String;
+  title: String;
+  content: String;
+  author: String;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+interface Props extends Post {
   children?: React.ReactNode;
 }
 
@@ -8,11 +16,11 @@ const PostContainer: React.FC<Props> = props => {
   return (
     <div className={styles.postContainer}>
       <div className={styles.header}>
-        <span className={styles.author}>{props.user}</span>
-        <span className={styles.date}>{props.date}</span>
+        <span className={styles.author}>{props.author}</span>
+        <span className={styles.date}>{String(props.createdAt)}</span>
       </div>
       <h3 className={styles.title}>{props.title}</h3>
-      <p className={styles.body}>{props.body}</p>
+      <p className={styles.body}>{props.content}</p>
     </div>
   );
 };
