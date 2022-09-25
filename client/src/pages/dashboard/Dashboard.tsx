7eae77875c1e852cf78cb24180b0a8a6af378697
DragonSky_NG <dragonskyng@gmail.com>
2022-09-25 16:59:56 +0300

    Resovle merge conflict

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,19 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './dashboard.module.scss';
-import { useContext } from 'react';
-import { LoggedInContext } from '../../contexts/LoggedInContext';
-import { NavLink } from 'react-router-dom';
-const Dashboard: React.FC = () => {
-  const loginCtx = useContext(LoggedInContext);
-
-  if (!loginCtx!.login.isLoggedIn) {
-    return (
-      <div>
-        <p>You need to login</p>
-        <NavLink to="/login" >Go to login</NavLink>
-      </div>
-    );
-  }
 
 const Dashboard: React.FC = () => {
   return (
